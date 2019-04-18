fa964f3266257506e6911cbb01283e292dc6bce5
rtcas-isteiul <rtcas@iscte-iul.pt>
2019-04-18 18:50:07 +0100

    guardar cenas para a mongo (comigo nao funciona)

--- SID_32/src/mongo/InsertToMongoTesteRita.java
+++ SID_32/src/mongo/InsertToMongoTesteRita.java
@@ -1,19 +1,27 @@
 package mongo;
 
+import org.bson.BsonArray;
 import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken;
 import org.eclipse.paho.client.mqttv3.MqttCallback;
 import org.eclipse.paho.client.mqttv3.MqttClient;
+import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
 import org.eclipse.paho.client.mqttv3.MqttException;
 import org.eclipse.paho.client.mqttv3.MqttMessage;
 
+import com.mongodb.BasicDBList;
 import com.mongodb.DB;
 import com.mongodb.DBCollection;
+import com.mongodb.DBCursor;
+import com.mongodb.DBObject;
 import com.mongodb.MongoClient;
 import com.mongodb.client.MongoCursor;
+import com.mongodb.util.JSON;
 
 public class InsertToMongoTesteRita implements MqttCallback{
 	MqttClient client;
-	
+	DB db;
+	DBCollection collection;
+//	BasicDBList dbList;
 	public InsertToMongoTesteRita() {
 		MongoClient mongoClient = new MongoClient();
 		
@@ -22,8 +30,9 @@ public class InsertToMongoTesteRita implements MqttCallback{
 		    System.out.println(dbsCursor.next());
 		}
 		
-		DB db = mongoClient.getDB("sensores");
-		DBCollection collection = db.getCollection("sensor");
+		db = mongoClient.getDB("sensores");
+		collection = db.getCollection("sensor");
+	//	dbList = new BasicDBList();
 	}
 	
 	
@@ -38,7 +47,12 @@ public class InsertToMongoTesteRita implements MqttCallback{
 		try {
 			client = new MqttClient("wss://iot.eclipse.org:443/ws", "user321");
 			
-			client.connect();
+			MqttConnectOptions options = new MqttConnectOptions();
+			options.setAutomaticReconnect(true);
+			options.setCleanSession(true);
+			options.setConnectionTimeout(10);
+			client.connect(options);
+			
 		    client.setCallback(this);
 		    client.subscribe("iscte_sid_2016_S1");
 		} catch (MqttException e) {
@@ -62,7 +76,46 @@ public class InsertToMongoTesteRita implements MqttCallback{
 	@Override
 	public void messageArrived(String topic, MqttMessage message) throws Exception {
 		// TODO Auto-generated method stub
-		 System.out.println(message);  
+				 saveMessage(message);
+				 System.out.println(message); 
+
+		 
 		
+	}
+	
+	public void saveMessage (MqttMessage msg){
+		//nova tentativa	
+		
+		System.out.println(msg.toString() + "   oi");
+//		BsonArray parse = BsonArray.parse(msg.toString());
+//		System.out.println( "       estou aqui");
+//		BasicDBList dbList =new BasicDBList();
+//		dbList.addAll(parse);
+//		DBObject dbObject = dbList;
+//		System.out.println("cheguei aqui?");
+		
+		
+		//antigo
+		
+	//DBObject dbObject = (DBObject)JSON.parse(msg.toString());
+	System.out.println("cheguei aquiiiiiiiiiiiiiiiiiii");
+	//	collection.insert(dbObject);
+//		DBCursor cursorDocJSON = collection.find();
+//		while (cursorDocJSON.hasNext()) {
+//			System.out.println(cursorDocJSON.next());
+//		}
+
+	}
+	public boolean checkMsgFormat (String msg) {
+
+		return msg.matches("\\{(\"tmp\"(\\s+)?:(\\s+)?\\d+.\\d+,(\\s+)?)"
+				+ "\"hum\"(\\s+)?:(\\s+)?\\d+.\\d+,(\\s+)?"
+				+ "\"dat\"(\\s+)?:(\\s+)?\"\\d+-(0?[1-9]|1[012])-(0?[1-9]|[12][0-9]|3[01])\",(\\s+)?"
+				+ "\"tim\"(\\s+)?:(\\s+)?\"(0?[0-9]|1[0-9]|2[0-3]):([0-5][0-9]):([0-5][0-9])\",(\\s+)?"
+				+ "\"cell\"(\\s+)?:(\\s+)?\\d+,(\\s+)?"
+				+ "\"sens\"(\\s+)?:(\\s+)?\"\\w+\",(\\s+)?"
+				+ "\"exported\"(\\s+)?:(\\s+)?[01](\\s+)?\\}");
+			
+				
 	}
 }
