917e6384d57c13ca235c0e33233d322a989a46cc
rtcas-isteiul <rtcas@iscte-iul.pt>
2019-04-18 22:07:06 +0100

    Create SensorInfo.java

---
+++ SID_32/src/mongo/SensorInfo.java
@@ -0,0 +1,18 @@
+package mongo;
+
+import java.util.Date;
+
+public class SensorInfo {
+	private double temp;
+	private double hum;
+	private String dat;
+	private String tim;
+	private int cell;
+	private String sens;
+	
+	public SensorInfo(String [] s) {
+		super();
+	}
+	
+	
+}
