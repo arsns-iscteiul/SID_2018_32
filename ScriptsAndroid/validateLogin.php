cc3dbb44cfaecf7af53e3297499481cdb28c7140
rtcas-isteiul <rtcas@iscte-iul.pt>
2019-05-14 15:04:22 +0100

    scripts android

--- ScriptsAndroid/validateLogin.php
+++ ScriptsAndroid/validateLogin.php
@@ -1,9 +1,8 @@
 	<?php
 	$url="127.0.0.1";
-	$database="main";
+	$database="sid2019";
     $conn = mysqli_connect($url,$_POST['username'],$_POST['password'],$database);
 	$response["valid"] = array();
 	$json = json_encode($response["valid"]);
 	echo $json;
 	mysqli_close ($conn);
-	?> 
