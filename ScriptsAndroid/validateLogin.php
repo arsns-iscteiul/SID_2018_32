64e77ca5770bddd973cbd2b7ab6be753b14e7cad
ljcra <ljcra@iscte-iul.pt>
2019-05-16 11:15:17 +0100

    LastChanges2

--- ScriptsAndroid/validateLogin.php
+++ ScriptsAndroid/validateLogin.php
@@ -1,6 +1,6 @@
 	<?php
 	$url="127.0.0.1";
-	$database="sid2019";
+	$database="main";
     $conn = mysqli_connect($url,$_POST['username'],$_POST['password'],$database);
 	$response["valid"] = array();
 	$json = json_encode($response["valid"]);
